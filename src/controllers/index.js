c0676a68271544c0a22b1fc8f7221e1c5a91828c
MuhammadAsif46 <asifahmed32c@gmail.com>
2025-02-13 23:57:31 +0500

    add getAll user controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,11 +1,32 @@
-const getAllUser = (req, res) => {};
+const getAllUser = async (req, res) => {
+  try {
+    const users = await db.query("SELECT * FROM users ORDER BY id DESC");
+
+    if (!users) {
+      return res.status(404).send({
+        message: "No users found",
+      });
+    }
+
+    res.send({
+      message: "users fetched",
+      data: users[0],
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "Internal server error",
+      error: error?.message,
+    });
+  }
+};
+const getUserById = (req, res) => {};
 const AddUser = (req, res) => {
   const { firstName, lastName, email, phone } = req;
   console.log(firstName, lastName, email, phone);
-  
 };
 const UpdateUser = (req, res) => {};
 const DeleteUser = (req, res) => {};
-const getUserById = (req, res) => {};
 
 export { getAllUser, AddUser, UpdateUser, DeleteUser, getUserById };
+
